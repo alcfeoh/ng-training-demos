eafbb66c8b0192d8d588e53319181ced3b57fe01
alcfeoh <alain@interstate21.com>
2026-01-26 17:38:16 +0100

    Upgrade to v21.1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import 'zone.js';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
   .catch((err) => console.error(err));
